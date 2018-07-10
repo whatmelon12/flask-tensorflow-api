d0d1a3d7bc44178cd595d12d4a9739adfa1511b2
Omar Jesus Mejia <omargotico@hotmail.com>
2018-07-09 21:41:54 -0500

    Added CORS to web API

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, abort, jsonify
+from flask_cors import CORS
 from utilities.util import Util
 from tensor.image_classifier import ImageClassifier
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
